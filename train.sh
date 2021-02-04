8acfa47cc16eda15d9d41b157cb1b4194caa4ed8
Giacomo Melacini <gmelacini@unibz.it>
2021-02-04 17:40:21 +0100

    Added bash training script

---
+++ train.sh
@@ -0,0 +1,8 @@
+python3 alpha_zero_trainer.py \
+--agent "hand_watten_train_cnn" \
+--workspace "versions/hand_watten/training/cnn/test" \
+--max_steps 400 \
+--games_num 100 \
+--iterations 10 \
+--exploration_decay_steps 15 \
+--epochs 1
