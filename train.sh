5a71fadca0ab2f27db4f583a026b324ed6131c53
Melacini Giacomo (Student Com17) <gmelacini@slurm-ctrl.inf.unibz.it>
2021-02-04 19:03:43 +0100

    Changed train bash script

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python3 alpha_zero_trainer.py \
 --agent "hand_watten_train_cnn" \
 --workspace "versions/hand_watten/training/cnn/test" \
 --max_steps 400 \
---games_num 100 \
+--games_num 10 \
 --iterations 10 \
 --exploration_decay_steps 15 \
 --epochs 1
