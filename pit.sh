9962a29319a26694978008e40056bb1753b8df53
Melacini Giacomo (Student Com17) <gmelacini@slurm-ctrl.inf.unibz.it>
2021-02-23 15:01:03 +0100

    Added pit bash script

---
+++ pit.sh
@@ -0,0 +1,8 @@
+python3 pit.py \
+--agent_new "hand_watten_evaluate" \
+--agent_old "hand_watten_evaluate_cnn" \
+--games_num 10000 \
+--agent_new_path "versions/hand_watten/training/ffnn/card/model_updated_599.h5" \
+--agent_old_path "versions/hand_watten/training/cnn/card/model_updated_599.h5" \
+--verbose \
+--debug
