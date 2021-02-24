1b0e01fda913fb762e71c624cecb1336bea13126
Melacini Giacomo (Student Com17) <gmelacini@slurm-ctrl.inf.unibz.it>
2021-02-24 18:06:56 +0100

    Changed scripts

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,5 @@
 python3 evaluate_models_in_folder.py \
 --agent "hand_watten_evaluate_cnn" \
---folder "versions/hand_watten/training/cnn/card" \
+--folder "versions/hand_watten/training/cnn/raise" \
 --games_num 10000 \
 --random_agent "agent_random"
--- previous.sh
+++ previous.sh
@@ -1,6 +1,6 @@
 python3 evaluate_model_against_previous.py \
---agent "hand_watten_evaluate" \
---folder "versions/hand_watten/training/ffnn/raise/previous" \
+--agent "hand_watten_evaluate_cnn" \
+--folder "versions/hand_watten/training/cnn/raise/previous" \
 --games_num 1000 \
 --random_agent "agent_random" \
---name "ffnn_raise_previous.csv"
+--name "cnn_raise_previous.csv"
