a1e161dadcc463ba98fd22f696b9c303d911a73f
diego loiacono <diegoloiac@live.it>
2022-04-10 17:41:00 +0200

    multiproc

--- multiprocesstest/core/World.py
+++ multiprocesstest/core/World.py
@@ -229,7 +229,7 @@ class World:
 
         for id_loop in loop_range:
             with p.Pool(processes=8) as pool: 
-                game_experience, game_results =  pool.starmap(self.execute_game,[(
+                game_experience, game_results =  pool.imap(self.execute_game,[(
                                                               max_game_steps_n,
                                                               allow_exploration,verbose,
                                                               show_every_turn,
