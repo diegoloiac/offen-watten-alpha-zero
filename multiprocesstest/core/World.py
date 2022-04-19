fa984b32567cfde0c6f3639f0f4a1636b468d6f1
diego loiacono <diegoloiac@live.it>
2022-04-19 18:37:03 +0200

    multiproc

--- multiprocesstest/core/World.py
+++ multiprocesstest/core/World.py
@@ -228,12 +228,10 @@ class World:
         loop_range = tqdm(loop_range)
 
         for id_loop in loop_range:
-            with p.Pool(processes=2) as pool: 
-                game_experience, game_results =  pool.starmap(self.execute_game,[(
-                                                              max_game_steps_n,
+            game_experience, game_results = self.execute_game(max_game_steps_n,
                                                               allow_exploration,verbose,
                                                               show_every_turn,
-                                                              exploration_decay_steps)])
+                                                              exploration_decay_steps)
 
             for idx, result in enumerate(game_results):
                 if result > 0:
