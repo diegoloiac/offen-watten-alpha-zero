c217dfc14a251fb6162aa1645e64272c011c2438
diego loiacono <diegoloiac@live.it>
2022-05-09 19:04:19 +0200

    threadpool

--- multiprocesstest-threadpool/core/World.py
+++ multiprocesstest-threadpool/core/World.py
@@ -238,8 +238,7 @@ class World:
 
 
                 for future in futures:
-                    game_experience= future(0)
-                    game_results = future(1)
+                    game_experience, game_results = future.result()
 
                     for idx, result in enumerate(game_results):
                         if result > 0:
